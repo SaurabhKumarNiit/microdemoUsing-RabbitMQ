f9712fdc56c75d920650f97fd08162786004ad6f
Saurabh Kumar <sky.royaltiwari@gmail.com>
2022-12-02 12:06:11 +0530

    Complete

--- UserAuthenticationService/src/main/java/com/practice/UserAuthenticationService/domain/User.java
+++ UserAuthenticationService/src/main/java/com/practice/UserAuthenticationService/domain/User.java
@@ -37,7 +37,7 @@ public class User {
     @Override
     public String toString() {
         return "User{" +
-                "email='" + userId + '\'' +
+                "userId='" + userId + '\'' +
                 ", password='" + password + '\'' +
                 '}';
     }
